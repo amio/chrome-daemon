1a6fa8eec264f9b55e32dc5b1fcd411654361b88
Amio <amio.cn@gmail.com>
2017-07-25 14:47:39 +0800

    Clean default flags.

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const chromeLauncher = require('chrome-launcher')
 
 const instances = []
 
-function launch ({port = 9222}) {
+function launch ({port = 9222, flags}) {
   return chromeLauncher.launch({
     port: port,
-    chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox', '--hide-scrollbars']
+    chromeFlags: flags
   }).then(chrome => {
-    // console.log(`Chrome debugging port running on ${chrome.port}`)
     instances.push({port, chrome})
   })
 }
@@ -19,8 +18,8 @@ function kill ({port = 9222}) {
     : Promise.reject(new Error(`No chrome running on port ${port}`))
 }
 
-function restart ({port = 9222}) {
-  return kill({port}).then(launch({port}))
+function restart ({port = 9222, flags}) {
+  return kill({port}).then(launch({port, flags}))
 }
 
 module.exports = {
